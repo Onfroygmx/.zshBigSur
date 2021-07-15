0879d370302b8ccd10e701321cd27490ff1decaa
onf_mbp <onfroy@gmx.com>
2021-07-15 19:37:16 +0200

    Initial Commit

---
+++ .dot/install.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+#!/bin/zsh
+#   _____          _        _ _
+#  |_   _|        | |      | | |
+#    | | _ __  ___| |_ __ _| | |
+#    | || '_ \/ __| __/ _` | | |
+#   _| || | | \__ \ || (_| | | |
+#   \___/_| |_|___/\__\__,_|_|_|
+#
+# https://patorjk.com/software/taag/#p=display&c=bash&f=Doom&t=Install
+### Install basic tools
